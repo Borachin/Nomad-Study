5b69c26066aba748762418ba2b8152545271b4fe
BoraChin <chinbora92@naver.com>
2021-01-02 08:13:24 +0000

    Calculate function

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function sayHello(potato) {
-  console.log("Hello!",potato);
+// function sayHello(name, age) {
+//   return (`Hello ${name} you are ${age} years old`);
+// }
+
+// const HelloBora = sayHello("Bora",30);
+// console.log(HelloBora)
+
+const calculator = {
+  plus: function(a,b){
+    return a + b;
+  }
 }
 
-sayHello("Bora");
-console.log();
+// console.log(HelloBora)
+const plus = calculator.plus(6,3)
+console.log(plus)
